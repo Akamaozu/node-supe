bb77e36a6609ebc0b3125d429d0d58d3cd579a86
Uzo Olisemeka <uzo@designbymobius.ca>
2017-05-15 11:26:21 -0400

    extracted citizen instance creation to its own file

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-module.exports = require('./lib/supervisor');
+var is_citizen_instance = ( process && process.send );
 
-if( process && process.send ){
-
-  // is exporting to a citizen
-  // overload export with citizen-related utils
-    module.exports.supervised = true;
-    module.exports.use = load_citizen_module;
-
-  // load citizen core modules
-    module.exports.use( require('./lib/modules/citizen/signal') );
-    module.exports.use( require('./lib/modules/citizen/mail') );
-    module.exports.use( require('./lib/modules/citizen/supervisor-noticeboard-bindings') );
-}
-
-function load_citizen_module( loader ){
-
-  if( !loader ) return false;
-  if( typeof loader != 'function' ) throw new Error( 'module loader must be a function' );
-
-  loader( module.exports );
-}
+module.exports = is_citizen_instance ? require('./lib/citizen') : require('./lib/supervisor');
