2f2fca3e8e78d65579862f48761b3d7b6a0bc76a
Uzo Olisemeka <uzo@designbymobius.ca>
2017-12-23 21:38:56 -0500

    notice-receiving test citizens now send a signal when they are ready to receive notices

--- test/citizen/notice-once-receiver.js
+++ test/citizen/notice-once-receiver.js
@@ -1,8 +1,8 @@
 var supe = require('../../index');
 
 supe.noticeboard.once( 'sample-notice', 'send-reply-mail', function( msg ){
-  
   supe.mail.send({ notice: 'sample-notice', received: msg.notice });
 });
 
+supe.noticeboard.notify( 'ready-to-receive-notices', { citizen: supe.get_name() });
 setTimeout( process.exit, 10000 );
