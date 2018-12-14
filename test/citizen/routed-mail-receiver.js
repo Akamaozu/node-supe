4614971739b6ac9684c2f9b8f260be6f59e69d07
Uzo Olisemeka <uzo@designbymobius.ca>
2018-12-14 08:39:10 -0500

    update test citizen to handle only routed mail

--- test/citizen/routed-mail-receiver.js
+++ test/citizen/routed-mail-receiver.js
@@ -1,9 +1,9 @@
 var citizen = require('../../index');
 
 citizen.hook.add( 'citizen-mail', 'handle-routed-mail', function( envelope, ack ){
-  if( ! envelope.from ) console.log({ action: 'handle-routed-mail', success: false, error: 'no sender specified on envelope' });
-  else citizen.mail.send({ received_mail_from: envelope.from });
+  if( ! envelope.from ) return;
 
+  citizen.mail.send({ received_mail_from: envelope.from });
   ack();
 });
 
